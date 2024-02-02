50731cb002475bf6a71dfe33db8d4d256ebf00e6
Vincde <145802178+Vincde@users.noreply.github.com>
2024-02-02 16:29:33 +0100

    Add password field verification in javascript  + css

---
+++ script.js
@@ -0,0 +1,19 @@
+function getPasswordResult(){
+    const button1 = document.querySelector('button');
+    const pass1 = document.querySelector('#password');
+    const pass2 = document.querySelector('#confirm-password')
+
+    button1.addEventListener('click',() =>{
+        console.log('ciao');
+        if(pass1.value === pass2.value){
+            pass1.setAttribute('style','border:2px solid green');
+            pass2.setAttribute('style','border:2px solid green');
+        }else{
+            pass1.setAttribute('style','border:2px solid red');
+            pass2.setAttribute('style','border:2px solid red');
+        }
+    })
+
+}
+
+getPasswordResult();
